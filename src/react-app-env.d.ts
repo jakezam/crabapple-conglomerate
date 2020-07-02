569b4280229a878a0c1d9107a96938c357ec35c7
teetle <unitedbagels@gmail.com>
2020-07-02 16:58:24 -0400

    feat: Page creation

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -2,3 +2,8 @@
 
 // To solve the issue: https://github.com/DefinitelyTyped/DefinitelyTyped/issues/31245
 /// <reference types="styled-components/cssprop" />
+
+declare module '*.scss' {
+  export const content: { [className: string]: string };
+  export default content;
+}
--- src/types/RootState.ts
+++ src/types/RootState.ts
@@ -1,15 +1,11 @@
-import { GithubRepoFormState } from 'app/containers/GithubRepoForm/types';
-import { ThemeState } from 'styles/theme/types';
+import { SigninPageState } from 'app/containers/SigninPage/types';
 // [IMPORT NEW CONTAINERSTATE ABOVE] < Needed for generating containers seamlessly
 
 /* 
   Because the redux-injectors injects your reducers asynchronously somewhere in your code
   You have to declare them here manually
-  Properties are optional because they are injected when the components are mounted sometime in your application's life. 
-  So, not available always
 */
 export interface RootState {
-  theme?: ThemeState;
-  githubRepoForm?: GithubRepoFormState;
+  signinPage?: SigninPageState;
   // [INSERT NEW REDUCER KEY ABOVE] < Needed for generating containers seamlessly
 }
