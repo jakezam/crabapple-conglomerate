c4395d7307accb172ebb6c39470a6792c2cfa7a7
Jacob Zamani <44008726+jakezam@users.noreply.github.com>
2020-07-03 16:23:51 -0400

    feat: Base layout put in

--- src/app/containers/SellerProfilePage/index.tsx
+++ src/app/containers/SellerProfilePage/index.tsx
@@ -14,6 +14,7 @@ import { reducer, sliceKey } from './utils/slice';
 import { selectSellerProfilePage } from './utils/selectors';
 import { sellerProfilePageSaga } from './utils/saga';
 import './SellerProfilePage.scss';
+import { Image } from 'semantic-ui-react';
 
 interface Props {}
 
@@ -33,6 +34,25 @@ export function SellerProfilePage(props: Props) {
         <meta name="description" content="Description of SellerProfilePage" />
       </Helmet>
       <div className="header">Header</div>
+      <div className="profileContainer">
+        <div className="leftHalf">
+          <div className="profileInfo">
+            <div className="profileImageContainer">
+              <Image src="logo512.png" size="small"></Image>
+            </div>
+          </div>
+          <div className="porfolioContainer">Portfolio</div>
+          <div className="recentPostContainer">Recent Posts</div>
+          <div className="reviewContainer">Reviews</div>
+          <div className="aboutContainer">About</div>
+        </div>
+        <div className="rightHalf">
+          <div className="otherCategoryProfiles">Others</div>
+          <div className="previousJobs">Previous Jobs</div>
+          <div className="followingContainer">Following</div>
+        </div>
+      </div>
+      <div className="footer">Footer</div>
     </>
   );
 }
