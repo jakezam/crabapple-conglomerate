1259ab9d57b2abea3cb237a791f9c2f46cc3751a
Carl Spana <carl.spana8@gmail.com>
2020-07-15 11:54:47 -0400

    feat: finished the signup page

--- src/app/containers/Signup/index.tsx
+++ src/app/containers/Signup/index.tsx
@@ -28,49 +28,72 @@ export function Signup(props: Props) {
 
   return (
     <>
-      <div className="ui centered card" style={{ padding: 50, marginTop: 50 }}>
-        <form className="ui form">
-          <div className="field">
-            <label
-              style={{
-                fontFamily: "Lato,'Helvetica Neue',Arial,Helvetica,sans-serif",
-              }}
-            >
-              Email Address
-            </label>
-            <input placeholder="peepee@butthole.xxx" />
+      <div className="ui centered container" style={{ width: '30%' }}>
+        <div
+          className="ui fluid centered card"
+          style={{
+            padding: 50,
+            marginTop: 50,
+            fontFamily: "Lato,'Helvetica Neue',Arial,Helvetica,sans-serif",
+          }}
+        >
+          <form className="ui form">
+            <div className="field">
+              <label
+                style={{
+                  fontFamily:
+                    "Lato,'Helvetica Neue',Arial,Helvetica,sans-serif",
+                }}
+              >
+                Email Address
+              </label>
+              <input placeholder="example@gmail.com" />
+            </div>
+            <div className="field">
+              <label
+                style={{
+                  fontFamily:
+                    "Lato,'Helvetica Neue',Arial,Helvetica,sans-serif",
+                }}
+              >
+                Username
+              </label>
+              <input placeholder="Teetle" />
+            </div>
+            <div className="field">
+              <label
+                style={{
+                  fontFamily:
+                    "Lato,'Helvetica Neue',Arial,Helvetica,sans-serif",
+                }}
+              >
+                Password
+              </label>
+              <input></input>
+            </div>
+            <div className="field">
+              <label
+                style={{
+                  fontFamily:
+                    "Lato,'Helvetica Neue',Arial,Helvetica,sans-serif",
+                }}
+              >
+                Confirm Password
+              </label>
+              <input></input>
+            </div>
+            <button className="medium fluid positive ui button">
+              Create Account
+            </button>
+          </form>
+          <div
+            className="ui horizontal divider"
+            style={{ textTransform: 'lowercase' }}
+          >
+            Already have an account?
           </div>
-          <div className="field">
-            <label
-              style={{
-                fontFamily: "Lato,'Helvetica Neue',Arial,Helvetica,sans-serif",
-              }}
-            >
-              Username
-            </label>
-            <input placeholder="Teetle" />
-          </div>
-          <div className="field">
-            <label
-              style={{
-                fontFamily: "Lato,'Helvetica Neue',Arial,Helvetica,sans-serif",
-              }}
-            >
-              Password
-            </label>
-            <input></input>
-          </div>
-          <div className="field">
-            <label
-              style={{
-                fontFamily: "Lato,'Helvetica Neue',Arial,Helvetica,sans-serif",
-              }}
-            >
-              Confirm Password
-            </label>
-            <input></input>
-          </div>
-        </form>
+          <button className="ui fluid primary button">Sign in</button>
+        </div>
       </div>
     </>
   );
