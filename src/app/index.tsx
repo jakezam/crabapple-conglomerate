6782e101f101b3e0308a7b70b08e33e74c1a648a
Jacob Zamani <44008726+jakezam@users.noreply.github.com>
2020-10-04 11:58:02 -0400

    feat: Changed background color on Profile Page

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,28 +1,17 @@
-/**
- *
- * App
- *
- * This component is the skeleton around the actual pages, and should only
- * contain code that should be seen on all pages. (e.g. navigation bar)
- */
-
 import * as React from 'react';
 import { Helmet } from 'react-helmet-async';
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
-
-// import { NotFoundPage } from './components/NotFoundPage/Loadable';
 import 'semantic-ui-react';
 import { GlobalStyle } from 'styles/global-styles';
-
 import { HomePage } from './containers/HomePage/Loadable';
 import { NotFoundPage } from './components/NotFoundPage/Loadable';
-import { SellerProfilePage } from './containers/SellerProfilePage/Loadable';
-
 import { Signup } from './containers/Signup/Loadable';
 import { SignInPage } from './containers/SignInPage/Loadable';
 import { Header } from './components/Header';
 import { Footer } from './containers/Footer/Loadable';
+// import { Settings } from './containers/Settings/Loadable';
 import { Discover } from './containers/Discover/Loadable';
+import { SellerProfilePage } from './containers/SellerProfilePage/Loadable';
 
 export function App() {
   return (
@@ -36,10 +25,12 @@ export function App() {
 
       <Header />
       <Switch>
-        <Route exact path="/" component={SellerProfilePage} />
-        <Route exact path="/Discover" component={Discover} />
+        <Route exact path="/" component={HomePage} />
         <Route exact path="/signup" component={Signup} />
         <Route exact path="/login" component={SignInPage} />
+        <Route exact path="/discover" component={Discover} />
+        {/* <Route exact path="/settings" component={Settings} /> */}
+        <Route exact path="/profile-provider" component={SellerProfilePage} />
         <Route component={NotFoundPage} />
       </Switch>
       <GlobalStyle />
