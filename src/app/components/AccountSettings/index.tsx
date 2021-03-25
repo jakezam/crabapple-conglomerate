ef2636a0bf80f5332e8b342e520316c6205a3928
UnitedBagels <unitedbagels@gmail.com>
2021-03-25 17:27:12 -0400

    Initial settings page refactor

--- src/app/components/AccountSettings/index.tsx
+++ src/app/components/AccountSettings/index.tsx
@@ -6,6 +6,7 @@
 import React, { memo } from 'react';
 import styled from 'styled-components/macro';
 import { useTranslation } from 'react-i18next';
+import { Form } from 'semantic-ui-react'
 
 interface Props {}
 
@@ -13,7 +14,24 @@ export const AccountSettings = (props: Props) => {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const { t, i18n } = useTranslation();
 
-  return <Div>Account Settings</Div>;
+  return (
+    <>
+      <div>
+        <form className="ui form">
+          <div className="field">
+            <label
+              style={{
+                fontFamily:
+                  "Lato,'Helvetica Neue',Arial,Helvetica,sans-serif",
+              }}
+            >
+              Username
+            </label>
+          </div>
+        </form>
+      </div>
+    </>
+  )
 };
 
 const Div = styled.div``;
