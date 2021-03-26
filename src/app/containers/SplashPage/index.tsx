d3ec7323fde1601abdf1d4f59838a562c3d3a42b
NA <na.intj@pm.me>
2021-03-26 13:27:36 -0400

    refactor: removed redundant component

--- src/app/containers/SplashPage/index.tsx
+++ src/app/containers/SplashPage/index.tsx
@@ -1,32 +1,68 @@
-/**
- * SplashPage
- */
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import {
+  ButtonContainer,
+  Container,
+  Content,
+  FormBox,
+  ImageBox,
+} from './components/StyledComponents';
 import { useMediaQuery } from 'react-responsive';
+import { Button, Divider, Form } from 'semantic-ui-react';
+import { push } from 'connected-react-router';
 import { mobileMediaParameters } from '../../../utils/env.config';
-import { MobileSplashPage } from './views/MobileSplashPage';
-import { DesktopSplashPage } from './views/DesktopSplashPage';
-import styled from 'styled-components/macro';
+import { Header } from '../../components/Header';
+import { Footer } from '../../components/Footer';
 
-export function SplashPage() {
+export const SplashPage = () => {
+  const dispatch = useDispatch();
   const isMobile = useMediaQuery(mobileMediaParameters);
 
-  const DecideRender = () => {
-    return isMobile ? <DesktopSplashPage /> : <DesktopSplashPage />;
-  };
   return (
     <Container>
-      <DecideRender />
+      <Header />
+
+      <Content>
+        <FormBox>
+          <Form>
+            <Form.Field style={{ paddingRight: '1em' }}>
+              <label>Username</label>
+              <input placeholder="name@example.com" />
+            </Form.Field>
+            <Form.Field style={{ paddingRight: '1em' }}>
+              <label>Password</label>
+              <input placeholder="**************" />
+            </Form.Field>
+
+            <ButtonContainer>
+              <Button primary color="blue" style={{ width: '80%' }}>
+                Sign In
+              </Button>
+            </ButtonContainer>
+          </Form>
+
+          <Divider horizontal style={{ textTransform: 'lowercase' }}>
+            {"don't have an account?"}
+          </Divider>
+
+          <ButtonContainer>
+            <Button
+              style={{ width: '80%' }}
+              positive
+              color="green"
+              onClick={() => {
+                dispatch(push('/signup'));
+              }}
+            >
+              Create Account
+            </Button>
+          </ButtonContainer>
+        </FormBox>
+
+        {!isMobile && <ImageBox />}
+      </Content>
+
+      <Footer />
     </Container>
   );
-}
-
-const Container = styled.div`
-  width: 100vw;
-  height: 100vh;
-  display: flex;
-  flex: 1;
-
-  // DEBUG STYLES //
-  //border: solid 5px red;
-`;
+};
--- src/app/containers/SplashPage/views/MobileSplashPage.tsx
+++
@@ -1,3 +0,0 @@
-export const MobileSplashPage = () => {
-  return <div></div>;
-};
