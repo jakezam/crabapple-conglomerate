c42ae54baa87f2040f0534c97251b5a035943e68
UnitedBagels <unitedbagels@gmail.com>
2021-04-19 14:38:45 -0400

    feat: created privacy settings page with comm. format

--- src/app/components/PrivacySettings/index.tsx
+++ src/app/components/PrivacySettings/index.tsx
@@ -6,6 +6,7 @@
 import React, { memo } from 'react';
 import styled from 'styled-components/macro';
 import { useTranslation } from 'react-i18next';
+import { Message } from 'semantic-ui-react';
 
 interface Props {}
 
@@ -13,7 +14,38 @@ export const PrivacySettings = (props: Props) => {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const { t, i18n } = useTranslation();
 
-  return <Div>Privacy Settings</Div>;
+  return (
+    <div>
+      <form className="ui form">
+        <div className="field">
+          <div className="ui toggle checkbox">
+            <input type="checkbox" className="hidden"/>
+            <label>Enable email alerts from Providers</label>
+          </div>
+        </div>
+
+        <div className="field">
+          <div className="ui toggle checkbox">
+            <input type="checkbox" className="hidden"/>
+            <label>Enable text message alerts from Providers</label>
+          </div>
+        </div>
+
+        <div className="field">
+          <div className="ui toggle checkbox">
+            <input type="checkbox" className="hidden"/>
+            <label>Hide personal information from Providers </label>
+          </div>
+        </div>
+
+        <Message info>
+          <div className="header">Personal Information</div>
+          <p>Includes first name, last name, and phone number. Only your username will be displayed. Messages can only be sent directly through Inployd.</p>
+        </Message>
+
+      </form>
+    </div>
+  );
 };
 
 const Div = styled.div``;
